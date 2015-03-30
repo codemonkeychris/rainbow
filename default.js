d1d293f6386c23276b986f752645b1b30bfaf838
Chris Anderson <chrisan@outlook.com>
2015-03-29 22:56:05 -0700

    most horrible hack version of React for Babylon :)

--- default.js
+++ default.js
@@ -1,105 +1,165 @@
+function diff(master, newScene) {
+    if (!master) {
+        var keys = Object.keys(newScene.children);
+        for (var i=0; i<keys.length; i++) {
+            newScene.children[keys[i]].action = "create";
+        }
+        return newScene;
+    }
+    else {
+        var keys = Object.keys(master.children);
+        for (var i=0; i<keys.length; i++) {
+            var name = keys[i];
+            if (!newScene.children[name]) {
+                master.children[name].action = "delete";
+            }
+            else {
+                newScene.children[name].instance = master.children[name].instance;
+                master.children[name] = newScene.children[name];
+                master.children[name].action = "update";
+                newScene.children[name] = null;
+            }
+        }
+        var keys = Object.keys(newScene.children);
+        for (var i=0; i<keys.length; i++) {
+            var name = keys[i];
+            if (newScene.children[name]) {
+                newScene.children[name].action = "create";
+                master.children[name] = newScene.children[name];
+            }
+        }
+        return master;
+    }
+};
+function applyActions(dom, scene) {
+    var keys = Object.keys(dom.children);
+    for (var i=0; i<keys.length; i++) {
+        var name = keys[i];
+        var item = dom.children[name];
+        switch (item.action) {
+            case "create":
+                switch (item.type) {
+                    case "sphere":
+                        item.instance = BABYLON.Mesh.CreateSphere(name, item.segments || 16, item.size, scene);
+                        item.instance.position.x = item.x;
+                        item.instance.position.y = item.y;
+                        item.instance.position.z = item.z;
+                        break;
+                    case "hemisphericLight":
+                        item.instance = new BABYLON.HemisphericLight(name, new BABYLON.Vector3(0, 1, 0), scene);
+                        item.instance.intensity = item.intensity;
+                        break;
+                    case "pointLight":
+                        item.instance = new BABYLON.PointLight(name, new BABYLON.Vector3(item.x, item.y, item.z), scene);
+                        item.instance.diffuse = new BABYLON.Color3(item.diffuse.r, item.diffuse.g, item.diffuse.b);
+                        item.instance.specular = new BABYLON.Color3(item.specular.r, item.specular.g, item.specular.b);
+                        break;
+                }
+                item.action = null;
+                break;
+            case "update":
+                item.action = null;
+                switch (item.type) {
+                    case "sphere":
+                        item.instance.position.x = item.x;
+                        item.instance.position.y = item.y;
+                        item.instance.position.z = item.z;
+                        break;
+                    case "pointLight":
+                        item.instance.diffuse = new BABYLON.Color3(item.diffuse.r, item.diffuse.g, item.diffuse.b);
+                        item.instance.specular = new BABYLON.Color3(item.specular.r, item.specular.g, item.specular.b);
+                        item.instance.position.x = item.x;
+                        item.instance.position.y = item.y;
+                        item.instance.position.z = item.z;
+                        break;
+                    case "hemisphericLight":
+                        item.instance.intensity = item.intensity;
+                        break;
+                }
+                break;
+            case "delete":
+                throw "not implemented!!";
+                break;    
+        }
+    }
+};
+
 window.addEventListener("load", (function() {
     var canvas = document.getElementById("renderCanvas");
     var engine = new BABYLON.Engine(canvas, true);
 
-    var createScene = function () {
+    var lastDom = null;
+
+    var render = function (time) {
+        var root = { children: [] };
+        root.children["light1"] = {
+            type: 'hemisphericLight',
+            intensity: 0.7
+        };
+        root.children["light2"] = {
+            type: 'pointLight',
+            x: Math.abs(((time+80) % 120)/30 - 30), 
+            y:5, 
+            z:- 10,
+            diffuse: {r:.5, g:0, b:0},
+            specular: {r:1, g:0, b:0},
+        };
+
+        var size = .35;
+        var zsize=6;
+        var ysize=6;
+        var xsize=6;
+        for (var z=-zsize/2; z<=zsize/2; z++) {
+            for (var y=1; y<=ysize; y++) {
+                for (var x=-xsize/2; x<=xsize/2; x++) {
+                    var name = 'sphere('+x+','+y+','+z+')';
+                    var offset = (Math.abs((time % 60) - 30)/15)-1;
+                    root.children[name] = { 
+                        type:'sphere', 
+                        segments: 12,
+                        size:size, 
+                        x:offset + x*1.1*size, 
+                        y:y*1.1*size, 
+                        z:z*1.1*size
+                    };
+                }
+            }
+        }
+        return root;
+    };
+
+    var createScene = function (t) {
+
+        // This creates a basic Babylon Scene object (non-mesh)
         var scene = new BABYLON.Scene(engine);
 
-        var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 100, 100), scene);
-        var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 100, BABYLON.Vector3.Zero(), scene);
+        // This creates and positions a free camera (non-mesh)
+        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+
+        // This targets the camera to scene origin
+        camera.setTarget(BABYLON.Vector3.Zero());
+
+        // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        // Torus
-        var torus = BABYLON.Mesh.CreateTorus("torus", 8, 2, 32, scene, false);
-        torus.position.x = 25;
-        torus.position.z = 30;
+        lastDom = diff(lastDom, render(0));
+        applyActions(lastDom, scene);
 
-        var materialBox = new BABYLON.StandardMaterial("texture1", scene);
-        materialBox.diffuseColor = new BABYLON.Color3(0, 1, 0);//Green
-
-        // -----------------------------------------
-        // Creation of an easing animation within predefined easing functions
-        // ------------------------------------------
-
-        //Create a Vector3 animation at 30 FPS
-        var animationTorus = new BABYLON.Animation("torusEasingAnimation", "position", 30, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-
-        // the torus destination position
-        var nextPos = torus.position.add(new BABYLON.Vector3(-80, 0, 0));
-
-        // Animation keys
-        var keysTorus = [];
-        keysTorus.push({ frame: 0, value: torus.position });
-        keysTorus.push({ frame: 120, value: nextPos });
-        animationTorus.setKeys(keysTorus);
-
-        // Adding an easing function
-        // You can use :
-        //1.	CircleEase()
-        //2.	BackEase(amplitude)
-        //3.	BounceEase(bounces, bounciness)
-        //4.	CubicEase()
-        //5.	ElasticEase(oscillations, springiness)
-        //6.	ExponentialEase(exponent)
-        //7.	PowerEase(power)
-        //8.	QuadraticEase()
-        //9.	QuarticEase()
-        //10.	QuinticEase()
-        //11.	SineEase()
-        // And if you want a total control, you can use a Bezier Curve animation
-        //12.   BezierCurveEase(x1, y1, x2, y2)
-        var easingFunction = new BABYLON.CircleEase();
-
-        // For each easing function, you can choose beetween EASEIN (default), EASEOUT, EASEINOUT
-        easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
-
-        // Adding easing function to my animation
-        animationTorus.setEasingFunction(easingFunction);
-
-        // Adding animation to my torus animations collection
-        torus.animations.push(animationTorus);
-
-        //Finally, launch animations on torus, from key 0 to key 120 with loop activated
-        scene.beginAnimation(torus, 0, 120, true);
-
-        // ------------------------------------------
-        // Using Bezier curve to create a custom easing function
-        // See here to see some samples and values : http://cubic-bezier.com/
-        // -----------------------------------------
-
-        // Torus
-        var bezierTorus = BABYLON.Mesh.CreateTorus("torus", 8, 2, 32, scene, false);
-        bezierTorus.position.x = 25;
-        bezierTorus.position.z = 0;
-
-
-        // Create the animation
-        var animationBezierTorus = new BABYLON.Animation("animationBezierTorus", "position", 30, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-        var keysBezierTorus = [];
-        keysBezierTorus.push({ frame: 0, value: bezierTorus.position });
-        keysBezierTorus.push({ frame: 120, value: bezierTorus.position.add(new BABYLON.Vector3(-80, 0, 0)) });
-        animationBezierTorus.setKeys(keysBezierTorus);
-        var bezierEase = new BABYLON.BezierCurveEase(0.32, -0.73, 0.69, 1.59);
-        animationBezierTorus.setEasingFunction(bezierEase);
-        bezierTorus.animations.push(animationBezierTorus);
-        scene.beginAnimation(bezierTorus, 0, 120, true);
-
-        // ------------------------------------------
-        // Create a simple animation without easing functions
-        // ------------------------------------------
-
-        var torus0 = BABYLON.Mesh.CreateTorus("torus", 8, 2, 32, scene, false);
-        torus0.position.x = 25;
-        torus0.position.z = -30;
-        torus0.material = materialBox;
-
-        BABYLON.Animation.CreateAndStartAnimation("anim", torus0, "position", 30, 120,
-                 torus0.position, torus0.position.add(new BABYLON.Vector3(-80, 0, 0)));
+        // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+        var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 
         return scene;
-    }
+
+    };
 
     var scene = createScene();
+    var t = 0;
+
+    setInterval(function() {
+        t++;
+        lastDom = diff(lastDom, render(t));
+        applyActions(lastDom, scene);
+    }, 32);
 
     engine.runRenderLoop(function () {
       scene.render();
